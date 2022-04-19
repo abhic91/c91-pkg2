180bba3a55e6008b766ec36d0a53277a48d21bb1
Abhilash <abhilash.m@card91.io>
2022-04-19 10:48:04 +0530

    added fonts

--- packages/atoms/InputText/InputText.tsx
+++
@@ -1,8 +0,0 @@
-import { IInputTextProps } from "./InputText.types";
-import React from "react";
-
-const InputText = (props: IInputTextProps) => {
-  return <input type="text" />;
-};
-
-export default InputText;
--- packages/atoms/InputText/InputText.types.ts
+++
@@ -1,3 +0,0 @@
-import { HTMLAttributes } from "react";
-
-export type IInputTextProps = HTMLAttributes<HTMLInputElement> & {};
